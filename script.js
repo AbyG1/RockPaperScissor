ca9f53f50509e4d05fb2e8dc4f1d8bf735503ade
Aby George <abygeorge133@gmail.com>
2023-03-14 19:29:43 +0530

    Create second function playRound

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function getComputerChoice() {
     let randomNumber = Math.ceil(Math.random() * 3);
     let computerChoice;
+    console.log(randomNumber);
     switch (randomNumber){
         case 1:
             computerChoice = "Rock";
@@ -13,7 +14,42 @@ function getComputerChoice() {
              break;   
         
     }
-    return console.log(computerChoice);
+    return computerChoice;
 
 }
-getComputerChoice();
+
+function playRound(playerSelection,computerSelection){
+   playerSelection = playerSelection.toLowerCase();
+   computerSelection = computerSelection.toLowerCase();
+   console.log(computerSelection);
+
+   if(playerSelection === computerSelection){
+       return "Its a tie!";
+    }
+   else{
+        if(playerSelection == "rock" && computerSelection == "paper"){
+            return "You Lose! Paper beats Rock";
+        }else if(playerSelection == "rock" && computerSelection == "scissors"){
+            return "You Won! Rock beats Scissors";
+        }else if(playerSelection == "paper" && computerSelection == "scissors"){
+            return "You Lose! Scissors beat Paper";
+        }else if(playerSelection == "paper" && computerSelection == "rock"){
+            return "You won! Paper beats Rock";
+        }else if(playerSelection == "scissors" && computerSelection == "rock"){
+            return "You Lose! Rock beats Scissors";
+        }else if(playerSelection == "scissors" && computerSelection == "paper"){
+            return "You won! Scissors beat Paper";
+        }else {
+            return "invalid input";
+        }
+    }
+   
+}
+
+
+
+let computerSelection = getComputerChoice();
+let playerSelection = prompt("Rock,Paper,Scissors. Enter your choice in words.");
+console.log(playRound(playerSelection,computerSelection));
+
+
